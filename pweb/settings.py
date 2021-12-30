52b53f9bb380041ebfc2e5c72e2887751b5b7f3f
Mary <60517816+csfj87@users.noreply.github.com>
2021-12-30 16:27:31 -0500

    Update settings.py
    Updating Links

--- pweb/settings.py
+++ pweb/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +126,3 @@ MAILCHIMP_EMAIL_LIST_ID = "4e841548df"
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STSTICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
